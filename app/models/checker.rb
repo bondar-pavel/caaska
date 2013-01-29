273a86d900dbcee6fd279490ba2576800f9ecb1e
Pavel Bondar <pasha117@gmail.com>
2013-01-29 15:30:49 +0300

    Adding ckecker. Don't work for now

---
+++ app/models/checker.rb
@@ -0,0 +1,29 @@
+class Checker
+#   require 'net/ssh'
+
+   def check_credential(unit, creds)
+       if (creds.nil?)
+            if Device.restond_to?(password) and Device.repond_to?(usename)
+               username = unit.username;
+               password = unit.password;
+            end
+       else
+            username = creds.usename;
+            password = creds.password;
+       end
+
+       if (Device.respond_to?(ip) and not username.blank?)
+           begin
+               Net::SSH.start( unit.ip, username, :password => password) do|ssh|
+	          # nothing to do for now
+               end
+               'Successfully authenticated'
+           rescue Net::SSH::AuthenticationFailed => e
+               "Invalid Credentials: #{e}"
+           rescue Net::SSH::SocketError => e
+               "Invalid Host: #{e}"
+           end
+       end
+   end
+
+end
