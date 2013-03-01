e2179f6a4793527927b17aea5523892c6f28388c
Pavel Bondar <pasha117@gmail.com>
2013-03-02 01:33:04 +0300

    Added ip validation and converting ip to numeric with apropriate unit tests.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,11 +1,27 @@
 class Network < ActiveRecord::Base
   attr_accessible :end_ip, :start_ip
 
-  def self.ip2num(ip = nil)
-      ip
+  def self.validate_ip(ip)
+      if(ip =~ /(\d+)\.(\d+)\.(\d+)\.(\d+)/)
+          if($1.to_i<256 && $2.to_i<256 && $3.to_i<256)
+              [$1.to_i, $2.to_i, $3.to_i, $4.to_i]
+          else
+              false
+          end
+      else
+          false
+      end
   end
 
-  def self.num2ip(num = nil)
-      num
+  def self.ip2num(ip)
+      if(arr = self.validate_ip(ip))
+         arr[0]*2**24 + arr[1]*2**16 + arr[2]*2**8 +arr[3]
+      else
+         nil
+      end
+  end
+
+  def self.num2ip(num)
+      num 
   end
 end
--- test/unit/network_test.rb
+++ test/unit/network_test.rb
@@ -4,10 +4,21 @@ class NetworkTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "ip validation" do
+      assert Network.validate_ip('192.168.13.23')
+  end
+
+  test "ip validation of wrong ip" do
+      assert !Network.validate_ip('Hi body!')
+  end
+
+  test "ip validation of wrong ip with exceeded numbers" do
+      assert !Network.validate_ip('220.156.257.12')
+  end
 
   test "ip to number conversation" do
       numeric = Network.ip2num('192.168.0.0')
-      num = 192*2**24 + 168*2**16 + 0 + 0
+      num = 192*2**24 + 168*2**16
       assert numeric == num, "Returned number #{numeric} should be #{num}"
   end
 
@@ -17,7 +28,8 @@ class NetworkTest < ActiveSupport::TestCase
   end
 
   test "number to ip conversation" do
-      pending "implement this part"
-      assert true
+      num = 172*2**24 + 23*2**16 + 10*2**8 + 15
+      numeric = Network.num2ip(num)
+      assert numeric == '172.23.10.15', "Returned ip #{numeric} should be 172.23.10.15"
   end
 end
